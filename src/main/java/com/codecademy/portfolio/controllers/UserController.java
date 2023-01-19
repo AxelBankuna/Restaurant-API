d6eb9b9d04045f4793a172b17b553ec502dbcb6c
AxelBankuna <axe.618@gmail.com>
2023-01-19 20:05:26 +0200

    updateUser in UserController

--- src/main/java/com/codecademy/portfolio/controllers/UserController.java
+++ src/main/java/com/codecademy/portfolio/controllers/UserController.java
@@ -2,9 +2,9 @@ package com.codecademy.portfolio.controllers;
 
 import com.codecademy.portfolio.models.User;
 import com.codecademy.portfolio.repositories.UserRepository;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/users")
@@ -16,8 +16,21 @@ public class UserController {
         this.userRepository = userRepository;
     }
 
-    @PostMapping
+    @PostMapping()
     public User createUser(User newUser) {
         return this.userRepository.save(newUser);
     }
+
+    @PutMapping("/{username}")
+    public User updateUser(@PathVariable("username") String username, User userUpdate) {
+        Optional<User> userToUpdateOptional = this.userRepository.findById(username);
+
+        if (userToUpdateOptional.isPresent()) {
+            userUpdate.setUsername(userToUpdateOptional.get().getUsername());
+            return this.userRepository.save(userUpdate);
+        }
+        return null;
+    }
+
+
 }
