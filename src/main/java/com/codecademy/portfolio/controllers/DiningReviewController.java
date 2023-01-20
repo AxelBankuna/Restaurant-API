34a61dd657b3c604473b97538a194ba89fdcf6da
AxelBankuna <axe.618@gmail.com>
2023-01-20 19:57:22 +0200

    getPendingReviews in DRController

--- src/main/java/com/codecademy/portfolio/controllers/DiningReviewController.java
+++ src/main/java/com/codecademy/portfolio/controllers/DiningReviewController.java
@@ -1,10 +1,16 @@
 package com.codecademy.portfolio.controllers;
 
 import com.codecademy.portfolio.models.DiningReview;
+import com.codecademy.portfolio.models.Status;
 import com.codecademy.portfolio.models.User;
 import com.codecademy.portfolio.repositories.DiningReviewRepository;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
 @RestController("/reviews")
 public class DiningReviewController {
@@ -15,10 +21,17 @@ public class DiningReviewController {
         this.diningReviewRepository = diningReviewRepository;
     }
 
-    @PostMapping("/user/{id}")
+    @PostMapping("/review/user/{id}")
     public DiningReview submitReview(User user, DiningReview review) {
         review.setUsername(user.getUsername());
 
         return this.diningReviewRepository.save(review);
+    }
+
+    @GetMapping("/pending")
+    public List<DiningReview> getPendingReviews() {
+        return StreamSupport.stream(this.diningReviewRepository.findAll().spliterator(), false)
+                .filter(review -> review.getStatus() == Status.PENDING)
+                .collect(Collectors.toList());
     }
 }
