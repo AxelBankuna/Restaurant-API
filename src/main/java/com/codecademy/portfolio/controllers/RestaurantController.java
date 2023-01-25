8b1dfac94c0e789bd6654c66598757a0ba635964
AxelBankuna <axe.618@gmail.com>
2023-01-25 18:31:18 +0200

    using ResponseEntity in UserController

--- src/main/java/com/codecademy/portfolio/controllers/RestaurantController.java
+++ src/main/java/com/codecademy/portfolio/controllers/RestaurantController.java
@@ -2,6 +2,7 @@ package com.codecademy.portfolio.controllers;
 
 import com.codecademy.portfolio.models.Restaurant;
 import com.codecademy.portfolio.repositories.RestaurantRepository;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -25,7 +26,7 @@ public class RestaurantController {
     public ResponseEntity<Restaurant> addRestaurant(@RequestBody Restaurant newRestaurant) {
         Iterable<Restaurant> allRestaurants = this.restaurantRepository.findAll();
         if (StreamSupport.stream(allRestaurants.spliterator(), false).anyMatch(
-                restaurant -> newRestaurant.equals(restaurant)
+                newRestaurant::equals
         )) {
             System.out.println("Restaurant already exists.");
             return null;
@@ -39,27 +40,61 @@ public class RestaurantController {
     }
 
     @GetMapping("{id}")
-    public Restaurant getRestaurant(@PathVariable("id") Long id) {
+    public ResponseEntity<Restaurant> getRestaurant(@PathVariable("id") Long id) {
         Optional<Restaurant> restaurantOptional = this.restaurantRepository.findById(id);
 
-        return restaurantOptional.orElse(null);
+        if (restaurantOptional.isPresent())
+            return new ResponseEntity<>(restaurantOptional.get(), HttpStatus.OK);
+        else {
+            HttpHeaders headers = new HttpHeaders();
+            headers.add("X-Error-Message",
+                    "Error: Restaurant not found with id: " + id);
+            return new ResponseEntity<>(null, headers, HttpStatus.NOT_FOUND);
+        }
     }
 
     @GetMapping("search")
-    public List<Restaurant> getRestaurantsByZipcodeAndAllergy(
+    public ResponseEntity<List<Restaurant>> getRestaurantsByZipcodeAndAllergy(
             @RequestParam(required = false) Integer zipcode,
             @RequestParam(required = false) String allergy) {
 
         if (Objects.nonNull(zipcode)) {
             if (Objects.nonNull(allergy)) {
                 return switch (allergy.toLowerCase()) {
-                    case "dairy" -> this.restaurantRepository.findAllByZipcodeAndDairyAverageIsNotNull(zipcode);
-                    case "peanut" -> this.restaurantRepository.findAllByZipcodeAndPeanutAverageIsNotNull(zipcode);
-                    case "egg" -> this.restaurantRepository.findAllByZipcodeAndEggAverageIsNotNull(zipcode);
-                    default -> this.restaurantRepository.findAllByZipcodeAndNonNullAverages(zipcode);
+                    case "dairy" -> new ResponseEntity<>(
+                            this.restaurantRepository.findAllByZipcodeAndDairyAverageIsNotNull(zipcode),
+                            HttpStatus.OK
+                    );
+                    case "peanut" -> new ResponseEntity<>(
+                            this.restaurantRepository.findAllByZipcodeAndPeanutAverageIsNotNull(zipcode),
+                            HttpStatus.OK
+                    );
+                    case "egg" -> new ResponseEntity<>(
+                            this.restaurantRepository.findAllByZipcodeAndEggAverageIsNotNull(zipcode),
+                            HttpStatus.OK
+                    );
+                    default -> new ResponseEntity<>(
+                            this.restaurantRepository.findAllByZipcodeAndNonNullAverages(zipcode),
+                            HttpStatus.OK
+                    );
                 };
             }
         }
-        return null;
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("X-Error-Message",
+                "Error: Unable to load list based on zipcode and allergy");
+        return new ResponseEntity<>(null, headers, HttpStatus.NOT_FOUND);
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<Restaurant> getRestaurantById(@PathVariable Long id) {
+        Optional<Restaurant> restaurant = this.restaurantRepository.findById(id);
+        HttpHeaders headers = new HttpHeaders();
+        if (restaurant.isPresent()) {
+            return new ResponseEntity<>(restaurant.get(), headers, HttpStatus.OK);
+        } else {
+            headers.add("X-Error-Message", "Error: Restaurant not found with id: " + id);
+            return new ResponseEntity<>(null, headers, HttpStatus.NOT_FOUND);
+        }
     }
 }
